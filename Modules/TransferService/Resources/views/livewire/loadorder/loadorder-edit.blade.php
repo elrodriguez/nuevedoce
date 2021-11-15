ecdda95a1fc4d140bcb4c8a4d35a743e6f0e421d
Elmer Rodriguez <elrodriguez2423@gmail.com>
2021-11-15 17:55:25 -0500

    ac

--- Modules/TransferService/Resources/views/livewire/loadorder/loadorder-edit.blade.php
+++ Modules/TransferService/Resources/views/livewire/loadorder/loadorder-edit.blade.php
@@ -1,11 +1,10 @@
 <div>
     <div class="card mb-g rounded-top">
+        <div class="card-header">
+            <h2 class="fw-700 m-0"><i class="subheader-icon fal fa-paper-plane"></i> @lang('transferservice::labels.lbl_odtlisting_detail'):</h2>
+        </div>
         <div class="card-body p-0">
             @if($this->departure_date == null or $this->departure_date == '')
-            <div class="form-row p-3">
-                <h2 class="fw-700 m-0"><i class="subheader-icon fal fa-paper-plane"></i> @lang('transferservice::labels.lbl_odtlisting_detail'):</h2>
-                <br>
-            </div>
             <div class="form-row">
                 <div class="col-md-12 mb-3">
                     <div class="table-responsive">
@@ -62,6 +61,13 @@
                 </div>
             </div>
             @endif
+        </div>
+    </div>
+    <div class="card mb-g rounded-top">
+        <div class="card-header">
+            <h2 class="fw-700 m-0"><i class="subheader-icon fal fa-people-carry"></i> @lang('transferservice::labels.lbl_load_order'):</h2>
+        </div>
+        <div class="card-body p-0">
             <form class="needs-validation {{ $errors->any()?'was-validated':'' }}" novalidate="">
                 <div class="form-row p-3">
                     <h2 class="fw-700 m-0"><i class="subheader-icon fal fa-people-carry"></i> @lang('transferservice::labels.lbl_load_order'):</h2>
