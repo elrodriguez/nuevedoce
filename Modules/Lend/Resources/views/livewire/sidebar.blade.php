ebfe568283366d41a85e9960eb2bbba85ddbe009
Elmer Rodriguez <elrodriguez2423@gmail.com>
2021-11-30 16:02:39 -0500

    info card user

--- Modules/Lend/Resources/views/livewire/sidebar.blade.php
+++ Modules/Lend/Resources/views/livewire/sidebar.blade.php
@@ -17,21 +17,7 @@
                 </a>
             </div>
         </div>
-        <div class="info-card">
-            <img src="{{ url('themes/smart-admin/img/demo/avatars/avatar-admin.png') }}" class="profile-image rounded-circle" alt="Dr. Codex Lantern">
-            <div class="info-card-text">
-                <a href="#" class="d-flex align-items-center text-white">
-                    <span class="text-truncate text-truncate-sm d-inline-block">
-                        {{ auth()->user()->name }}
-                    </span>
-                </a>
-                <span class="d-inline-block text-truncate text-truncate-sm">{{ auth()->user()->email }}</span>
-            </div>
-            <img src="{{ url('themes/smart-admin/img/card-backgrounds/cover-2-lg.png') }}" class="cover" alt="cover">
-            <a href="#" onclick="return false;" class="pull-trigger-btn" data-action="toggle" data-class="list-filter-active" data-target=".page-sidebar" data-focus="nav_filter_input">
-                <i class="fal fa-angle-down"></i>
-            </a>
-        </div>
+        <x-info-card-user></x-info-card-user>
         <ul id="js-nav-menu" class="nav-menu">
             @can('prestamos_dashboard')
             <li class="{{ $path[0] == 'lend' && $path[1] == 'dashboard' ? 'active' : '' }}">
