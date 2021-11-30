685ae81b9e82b120bdda3911c00b1fe02d89b643
Elmer Rodriguez <elrodriguez2423@gmail.com>
2021-11-30 12:01:30 -0500

    elmer

--- Modules/Staff/Http/Livewire/Sidebar.php
+++ Modules/Staff/Http/Livewire/Sidebar.php
@@ -3,9 +3,15 @@
 namespace Modules\Staff\Http\Livewire;
 
 use Livewire\Component;
-
+use Modules\Setting\Entities\SetCompany;
 class Sidebar extends Component
 {
+    public $company;
+
+    public function mount(){
+        $this->company = SetCompany::where('main',true)->first();
+    }
+
     public function render()
     {
         return view('staff::livewire.sidebar');
